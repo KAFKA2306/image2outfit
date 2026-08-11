5e826df47219737f43d82a8ff169cb551ef21ad1
KAFKA <137051370+KAFKA2306@users.noreply.github.com>
2026-08-11 17:02:13 +0900

    Add wardrobe camera and pose presets

---
+++ Assets/GenWorks/Shared/Editor/GenWorksPreviewPreset.cs
@@ -0,0 +1,30 @@
+#if UNITY_EDITOR
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GenWorks.Editor
+{
+    [CreateAssetMenu(
+        fileName = "GenWorksPreviewPreset",
+        menuName = "GenWorks/Wardrobe Preview Preset"
+    )]
+    internal sealed class GenWorksPreviewPreset : ScriptableObject
+    {
+        [Serializable]
+        internal sealed class TransformPose
+        {
+            public string path;
+            public Vector3 localPosition;
+            public Quaternion localRotation = Quaternion.identity;
+            public Vector3 localScale = Vector3.one;
+        }
+
+        public Vector2 cameraOrbit = new Vector2(12f, 180f);
+        public float cameraDistance = 2.2f;
+        public float fieldOfView = 30f;
+        public Vector3 pivotOffset = new Vector3(0f, 1f, 0f);
+        public List<TransformPose> pose = new List<TransformPose>();
+    }
+}
+#endif
