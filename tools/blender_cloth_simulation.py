31429ab8e0934ddbf2d0e8bc9a126f8e9db5dcdf
kafka2306 <your_email@example.com>
2026-09-21 15:10:57 +0900

    feat: implement Lily Vapor Yukata outfit issue 673

--- tools/blender_cloth_simulation.py
+++ tools/blender_cloth_simulation.py
@@ -51,6 +51,15 @@ COMPONENTS: dict[str, tuple[str, ...]] = {
         "Nocturnal_Hakama_Side_L",
         "Nocturnal_Hakama_Side_R",
     ),
+    "siroino-lily-vapor-yukata": (
+        "Lily_Summer_Front_Under",
+        "Lily_Summer_Front_Over",
+        "Lily_Summer_Back",
+        "Lily_Sleeve_Rail_Outer_L",
+        "Lily_Sleeve_Rail_Inner_L",
+        "Lily_Sleeve_Rail_Inner_R",
+        "Lily_Sleeve_Rail_Outer_R",
+    ),
 }
 
 FRAME_END = {
@@ -63,6 +72,7 @@ FRAME_END = {
     "siroino-lunar-tech-hoodie": 30,
     "siroino-verdant-ranger-explorer-set": 30,
     "siroino-nocturnal-shrine-maiden-long-hakama-set": 32,
+    "siroino-lily-vapor-yukata": 32,
 }
 
 
